b285ba162a7f60e48e838be5d3b5deac2feddd51
Rodrigo Assirati Dias <rodrigodias@gmail.com>
2022-02-13 20:44:35 -0300

    Primeiro teste com registro de jogadores e rolagem de dados

--- server/server.js
+++ server/server.js
@@ -10,9 +10,25 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (sock) => {
-    sock.emit('message', 'Você está conectado...');
 
     sock.on('message', (text) => io.emit('message', text));
+
+    sock.on('rollDice', () => {
+        io.emit('diceRolled', {
+            dice1: rollDice(),
+            dice2: rollDice(),
+            dice3: rollDice(),
+            dice4: rollDice(),
+            dice5: rollDice(),
+          });
+    });
+
+    sock.on('newPlayer', (playerName) => {
+        sock.emit('message', 'Você está conectado...');
+        io.emit('message', playerName + ' entrou no jogo!');
+        io.emit('message', 'Aguardando mais jogadores entrarem...');
+        io.emit('close modal');
+    });
 });
 
 server.on('error', (err) => {
@@ -22,3 +38,9 @@ server.on('error', (err) => {
 server.listen(8080, () => {
     console.log('Servidor pronto. Escutando a porta 8080...');
 });
+
+
+function rollDice() {
+    const number = Math.ceil(Math.random() * 6);
+    return number;
+  }
